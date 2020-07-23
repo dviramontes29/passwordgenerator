3104f88d6a388ca18b1647335cece7e1d389efae
David Viramontes <dviramontes29@yahoo.com>
2020-07-23 16:47:10 -0700

    small tweak (still not working properly)

--- index.js
+++ index.js
@@ -1,40 +1,66 @@
-// Assignment Code and Event Listener to prompt Q's when btn pushed
-var generatBtn = document.querySelector('#generate');
-generatBtn.addEventListener('click', createPassword);
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+function writePassword() {
+  let passLength = parseInt(prompt('Between 8 - 128, how long would you want your password?'))
+  if (passLength < 8 || passLength > 128) {
+    alert('Pick a Valid Number!')
+    return
+  }
 
 
+  let upperConfirm = confirm('Would you like uppercase?')
+  let lowerConfirm = confirm('Would you like lowercase?')
+  let numberConfirm = confirm('Would you like numbers?')
+  let specialConfirm = confirm('Would you like special characters?')
+  let characters = {
+    upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    special: ['+', '-', '&', '!', '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':']
+  }
 
-function createPassword() {
-    let passwordLenth = parseInt(prompt('what length do you want your password'))
-    if (passwordLenth <8 || passwordLenth > 128) {
-        alert('Please use valid number')
-        return
+
+  let password = ''
+  for (let i = 0; i < passLength; i++) {
+    let pick = Math.floor(Math.random() * 4)
+    if (pick === 0) {
+      if (upperConfirm === true) {
+        let change = characters.upperCase[Math.floor() * characters.upperCase.length]
+        password = password + change
+      } else {
+        i--
+      }
+    } else if (pick === 1) {
+      if (lowerConfirm === true) {
+        let change = characters.lowerCase[Math.floor() * characters.lowerCase.length]
+        password = password + change
+      } else {
+        i--
+      }
+    } else if (pick === 2) {
+      if (numberConfirm === true) {
+        let change = characters.numbers[Math.floor() * characters.numbers.length]
+        password = password + change
+      } else {
+        i--
+      }
+    } else {
+      if (specialConfirm === true) {
+        let change = characters.special[Math.floor() * characters.special.length]
+        password = password + change
+      } else {
+        i--
+      }
+
+document.getElementById("password")
+      
     }
-    let lowerCase = 'abc'
-    let upperCase = 'ABC'
-    let specChar = '!@#'
-    let numeric = '123'
-    let randoPass = ''
-    let lowerConfirm = confirm('do you want lower')
-    let upperConfirm = confirm('do you want upper')
-    let speConfirm = confirm('do you want special')
-    let numsConfirm = confirm('do you want numbers')
-    let selectedChars = ''
-
-    if (lowerConfirm) {
-        selectedChars += lowerChar
-    }
-    if (upperConfirm) {
-        selectedChars += upperChar
-    }
-    if (specConfirm) {
-        selectedChars += specChar
-    }
-    if (numsConfirm) {
-        selectedChars += numeric
-    }
-
-let passwordLenth = ''
-
-for(passwordLenth>7 || passwordLenth<128; passwordLenth++);
+  }
 }
+// Add event listener to generate button
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
